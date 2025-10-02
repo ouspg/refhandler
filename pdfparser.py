b86010646102e89698ba0f367f42cd9df61864e5
ttokola <teemu.tokola@oulu.fi>
2025-10-02 09:10:43 +0300

    Create pdfparser.py

---
+++ pdfparser.py
@@ -0,0 +1,17 @@
+
+# wake up, invoked by cron
+
+# Evaluate processor load? 
+
+# Fetch list of loaded PDF's that have not yet been parsed. 
+# Limit by load?
+
+# For all PDF's...
+
+# Extract raw text from PDF, insert to database
+
+# Find list of References from raw text
+
+# For all references:
+# 1. make an entry to database (OR if there already, fetch id)
+# 2. add an entry to database PDF->REF
