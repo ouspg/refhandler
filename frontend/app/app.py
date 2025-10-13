66929d601c01b551c78665dc6403957e89d5d188
LAKIpoe01 <laki.poe@gmail.com>
2025-10-13 18:49:00 +0300

    pdf_uploader siirretty backendiin

--- frontend/app/app.py
+++ frontend/app/app.py
@@ -46,7 +46,7 @@ async def upload_file(pdf_file: UploadFile = File()):
     files = {
         'pdf_file': (pdf_file.filename, await pdf_file.read(), pdf_file.content_type)
     }
-    res = requests.post("http://pdfuploader:8002/upload_pdf", files=files)
+    res = requests.post("http://backend:8001/upload_pdf", files=files)
     return res.json()
 
 if __name__ == "__main__":
