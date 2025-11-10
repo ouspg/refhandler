461ec66b3ae47022f56babf4a240d0634cc79b0b
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2025-11-10 16:11:08 +0200

    implemented multiple files per request

--- backend/app/api/routes/pdfs.py
+++ backend/app/api/routes/pdfs.py
@@ -8,18 +8,23 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 router = APIRouter()
 
-@router.post("/", response_model=PdfPublic)
-async def upload_pdf(session: SessionDep, pdf_file: UploadFile = File()):
-    if pdf_file.content_type != "application/pdf" or pdf_file.filename == None:
-        raise HTTPException(status_code=422, detail="Invalid PDF file")
-    
-    new_filename = uuid.uuid4()
-    file_location = os.path.join(UPLOAD_DIR, str(new_filename) + ".pdf")
-    with open(file_location, "wb") as f:
-        f.write(pdf_file.file.read())
-    
-    db_pdf = Pdf(id = new_filename, original_filename=pdf_file.filename)
-    session.add(db_pdf)
-    session.commit()
-    session.refresh(db_pdf)
-    return db_pdf
+@router.post("/")
+async def upload_pdf(session: SessionDep, files: list[UploadFile]):
+    return_files = []
+
+    for file in files:
+        if file.content_type != "application/pdf" or file.filename == None:
+            raise HTTPException(status_code=422, detail="Invalid PDF file")
+        
+        new_filename = uuid.uuid4()
+        file_location = os.path.join(UPLOAD_DIR, str(new_filename) + ".pdf")
+        with open(file_location, "wb") as f:
+            f.write(file.file.read())
+        
+        db_pdf = Pdf(id = new_filename, original_filename=file.filename)
+        session.add(db_pdf)
+        session.commit()
+        session.refresh(db_pdf)
+        return_files.append(db_pdf.model_copy(deep=True))
+
+    return return_files
--- backend/tests/api/test_pdfs.py
+++ backend/tests/api/test_pdfs.py
@@ -2,11 +2,24 @@ from fastapi.testclient import TestClient
 
 def test_post(client: TestClient):
     with open("backend/tests/api/test.pdf", "rb") as pdf_file:
-        response = client.post("/api/pdfs", files={'pdf_file': pdf_file})
+        response = client.post("/api/pdfs", files={'files': pdf_file})
         data = response.json()
         assert response.status_code == 200
-        assert data["original_filename"] == "test.pdf"
-        assert data["uploaded_by"] == None
+        for file in data:
+            assert file["original_filename"] == "test.pdf"
+            assert file["uploaded_by"] == None
+
+def test_post_multiple_files(client: TestClient):
+    with open("backend/tests/api/test.pdf", "rb") as pdf_file1,\
+            open("backend/tests/api/test.pdf", "rb") as pdf_file2:
+        files = [('files', pdf_file1), ('files', pdf_file2)]
+        
+        response = client.post("/api/pdfs", files=files)
+        data = response.json()
+        assert response.status_code == 200
+        for file in data:
+            assert file["original_filename"] == "test.pdf"
+            assert file["uploaded_by"] == None
         
 def test_post_with_missing_data(client: TestClient):
         response = client.post("/api/pdfs", files={'pdf_file': ""})
