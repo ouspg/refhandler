e789a1e888f5a43e64c05e0a5f86758d9d76f795
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2025-11-17 22:42:01 +0200

    Added database dump on startup

---
+++ postgres/dump_database.sh
@@ -0,0 +1,7 @@
+while ! pg_isready -U ${POSTGRES_USER} -d ${POSTGRES_DB};
+do
+    sleep 1
+done
+
+echo "Dumping database to /backups"
+pg_dump -c -U ${POSTGRES_USER} ${POSTGRES_DB} > backups/postgres_$(date -u +%Y-%m-%d-%H%M).sql
