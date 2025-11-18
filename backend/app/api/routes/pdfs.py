0b0ce28767f44de687f564200bbb92ed7ee40949
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2025-11-18 17:43:09 +0200

    Prepared scanners for multiple APIs. Comments

--- backend/app/api/routes/pdfs.py
+++ backend/app/api/routes/pdfs.py
@@ -1,8 +1,8 @@
 from fastapi import File, UploadFile, APIRouter, HTTPException
 from fastapi.responses import FileResponse
 import os, uuid
-from app.api.deps import SessionDep, ScannersDep
-from app.models.models import Pdf, PdfCreate, PdfPublic
+from app.api.depdendancies import SessionDep, ScannersDep
+from app.models import Pdf, PdfCreate, PdfPublic
 
 UPLOAD_DIR = os.environ.get("UPLOAD_DIR", 'NO UPLOAD_DIR IN ENVIRONMENT')
 os.makedirs(UPLOAD_DIR, exist_ok=True)
@@ -13,6 +13,7 @@ router = APIRouter()
 async def get_pdf(file_id: str):
     file = f"{UPLOAD_DIR}/{file_id}.pdf"
     
+    # Return {file_id}.pdf, if it exists on disk
     if os.path.isfile(file):
         return FileResponse(file, headers={"Content-Type": "application/pdf"})
     else:
@@ -20,18 +21,25 @@ async def get_pdf(file_id: str):
 
 @router.post("/", response_model=PdfPublic)
 async def upload_pdf(session: SessionDep, scanners: ScannersDep, pdf_file: UploadFile = File()):
+    # Reject files with wrong content type or no filename
+    # TODO: More checking. Check magic bytes?
     if pdf_file.content_type != "application/pdf" or pdf_file.filename == None:
         raise HTTPException(422, "Invalid PDF file")
     
-    scan_result = await scanners.clamav_scan(pdf_file)
+    # Scan file for viruses
+    scan_result = await scanners.scan(pdf_file)
     if(scan_result.status_code == 406):
-        raise HTTPException(406, f"ClamAV scan result: {pdf_file.filename} not safe")
+        raise HTTPException(406, {"scan_result": f"{pdf_file.filename} is not safe",
+                                  "msg": f"{scan_result}"
+                                  })
     
+    # File was found safe, save it to disk
     new_filename = uuid.uuid4()
     file_location = os.path.join(UPLOAD_DIR, str(new_filename) + ".pdf")
     with open(file_location, "wb") as f:
         f.write(pdf_file.file.read())
     
+    # Add file metadata to database
     db_pdf = Pdf(id = new_filename, original_filename=pdf_file.filename)
     session.add(db_pdf)
     session.commit()
@@ -42,6 +50,7 @@ async def upload_pdf(session: SessionDep, scanners: ScannersDep, pdf_file: Uploa
 async def get_file(file_id: str):
     file = f"{UPLOAD_DIR}/{file_id}.pdf"
     
+    # If {file_id}.pdf exists, delete it from disk
     if os.path.isfile(file):
         return os.remove(file)
     else:
--- backend/app/api/scanners.py
+++ backend/app/api/scanners.py
@@ -1,17 +1,39 @@
-from fastapi import UploadFile
+from fastapi import UploadFile, Response
 import requests
 import os
 
 CLAMAV_PORT = os.environ.get("CLAMAV_PORT", 'NO CLAMAV_PORT IN ENVIRONMENT')
 CLAMAV_SCAN_URL = f"http://clamav-rest:{CLAMAV_PORT}/v2/scan"
 
-class Scanners:
+# TODO: Add virustotal API. Skip if VIRUSTOTAL_API_KEY == ""
+# TODO: Refactor to run all scans with Scanners.scan()
 
-    @staticmethod
-    async def clamav_scan(file: UploadFile):
+# Class for calling file scanner APIs
+# If file is safe: return HTTP 200
+# else return HTTP 406
+class Scanners:
+    
+    # Scan files using locally hosted ClamAV-rest container
+    # https://github.com/ajilach/clamav-rest?tab=readme-ov-file#quick-start
+    async def clamav_scan(self, file: UploadFile):
         files = {
             'files': (file.filename, await file.read(), file.content_type)
         }
         
         response = requests.post(CLAMAV_SCAN_URL, files=files)
         return response
+    
+    # Run all scanners
+    async def scan(self, file: UploadFile):
+        responses = []
+        
+        # Run all scanners and store response in a list
+        responses.append(await self.clamav_scan(file))
+        
+        # Iterate over list and return HTTP 406 if any scanners flagged the file
+        for response in responses:
+            if response.status_code == 406:
+                return response
+            
+        # No scanners flagged the file, return HTTP 200
+        return Response()
