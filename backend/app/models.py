9ed971d1a8a78640158dec9fcea1511270f50b1f
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2025-11-24 01:21:53 +0200

    test migration undo

--- backend/app/models.py
+++ backend/app/models.py
@@ -12,7 +12,6 @@ from sqlmodel import Field, SQLModel
 class Post(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     db_text: str
-    test_column: str | None = None
     
 class PostCreate(SQLModel):
     db_text: str
