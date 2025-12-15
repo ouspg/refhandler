baf85a486009e8d4778ad2162c5b7bc246e5afc5
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2025-12-15 14:38:48 +0200

    integration test for /api/users

--- backend/tests/integration/test_apis.py
+++ backend/tests/integration/test_apis.py
@@ -2,10 +2,10 @@
 Integration tests for the Refhandler compose stack
 """
 # pylint: disable=missing-function-docstring, unused-argument, unused-import
+import os
 import pytest
 import requests
-import os
-from backend.app.models import UserCreate, PdfPublic
+from backend.app.models import UserCreate, UserUpdate, UserPublic, UserRole, PdfPublic
 
 
 test_email = "foo@bar.com"
@@ -32,18 +32,55 @@ def test_get_default_admin_token(api_url, docker_services):
     assert token_header is not None
 
 
-def test_signup(api_url, docker_services):
+def test_api_users(api_url, docker_services):
+    # Signup test_user
     response = requests.post(f"{api_url}/users/signup", json=test_user.model_dump(), timeout=20)
     assert response.status_code == 200
     assert response.json()["email"] == test_email
 
+    # Get token header for test_user
+    header = _get_access_token_header(api_url, test_email, test_password)
+    assert header is not None
+
+    # Get current user from api
+    response = requests.get(f"{api_url}/users/me", headers=header, timeout=20)
+    assert response.status_code == 200
+    assert response.json()["email"] == test_email
+
+    # Change current user email,role and password
+    new_email = "foofoo@barbar.com"
+    new_data = UserUpdate(email=new_email, role=UserRole.manager, password="new_password")
+
+    response = requests.patch(f"{api_url}/users/me",
+                            headers=header, json=new_data.model_dump(), timeout=20)
+    assert response.status_code == 200
+    updated_user = UserPublic.model_validate_json(response.text)
+    assert updated_user.email == new_email
+    assert updated_user.role == UserRole.manager
+
+    # Get test_user using user_id
+    response = requests.get(f"{api_url}/users/{updated_user.id}", headers=header, timeout=20)
+    assert response.status_code == 200
+    get_user = UserPublic.model_validate_json(response.text)
+    assert get_user.email == new_email
+
+    # Delete test_user
+    response = requests.delete(f"{api_url}/users/me", headers=header, timeout=20)
+    assert response.status_code == 200
+
+    # Make sure test_user was deleted
+    response = requests.delete(f"{api_url}/users/me", headers=header, timeout=20)
+    assert response.status_code == 404
+    response = requests.delete(f"{api_url}/users/{updated_user.id}", headers=header, timeout=20)
+    assert response.status_code == 404
+
 
 def test_api_pdfs(api_url, docker_services):
-    token_header = _get_access_token_header(api_url, ADMIN_EMAIL, ADMIN_PASSWORD)
+    header = _get_access_token_header(api_url, ADMIN_EMAIL, ADMIN_PASSWORD)
     with open("backend/tests/api/test.pdf", "rb") as pdf_file:
         # Upload test pdf file
         files = {"pdf_file": pdf_file}
-        response_post = requests.post(f"{api_url}/pdfs", files=files, headers=token_header, timeout=20)
+        response_post = requests.post(f"{api_url}/pdfs", files=files, headers=header, timeout=20)
 
         db_pdf = PdfPublic.model_validate_json(response_post.text)
         assert response_post.status_code == 200
@@ -51,25 +88,25 @@ def test_api_pdfs(api_url, docker_services):
 
         # Try to upload the same file again
         pdf_file.seek(0)
-        response_post_again = requests.post(f"{api_url}/pdfs", files=files, headers=token_header, timeout=20)
+        response_post_again = requests.post(f"{api_url}/pdfs", files=files, headers=header, timeout=20)
         assert response_post_again.status_code == 409
 
         # Get uploaded test file from backend and compare contents with original
-        response_get = requests.get(f"{api_url}/pdfs/{db_pdf.id}.pdf", headers=token_header, timeout=20)
+        response_get = requests.get(f"{api_url}/pdfs/{db_pdf.id}.pdf", headers=header, timeout=20)
         pdf_file.seek(0)
         assert pdf_file.read() == response_get.content
         assert response_get.headers["content-type"] == "application/pdf"
 
         # Get the pdf object from database
-        response_get = requests.get(f"{api_url}/pdfs/{db_pdf.id}", headers=token_header, timeout=20)
+        response_get = requests.get(f"{api_url}/pdfs/{db_pdf.id}", headers=header, timeout=20)
         assert response_get.status_code == 200
 
         # remove uploaded test file
-        response_delete = requests.delete(f"{api_url}/pdfs/{db_pdf.id}", headers=token_header, timeout=20)
+        response_delete = requests.delete(f"{api_url}/pdfs/{db_pdf.id}", headers=header, timeout=20)
         assert response_delete.status_code == 200
 
         # Make sure file was removed
-        response_after_delete = requests.get(f"{api_url}/pdfs/{db_pdf.id}.pdf", headers=token_header, timeout=20)
+        response_after_delete = requests.get(f"{api_url}/pdfs/{db_pdf.id}.pdf", headers=header, timeout=20)
         assert response_after_delete.status_code == 404
-        response_after_delete = requests.get(f"{api_url}/pdfs/{db_pdf.id}", headers=token_header, timeout=20)
+        response_after_delete = requests.get(f"{api_url}/pdfs/{db_pdf.id}", headers=header, timeout=20)
         assert response_after_delete.status_code == 404
